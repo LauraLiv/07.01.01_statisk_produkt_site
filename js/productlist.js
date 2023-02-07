ea7e3a7c625f1222d704ec8ea9036a9e84b4650d
Laura Pesando <laur491w@stud.kea.dk>
2023-02-07 11:36:46 +0100

    h

--- js/productlist.js
+++ js/productlist.js
@@ -1,3 +1,22 @@
 https: fetch("https://kea-alt-del.dk/t7/api/products")
-  .then((response) => response.json())
-  .then((data) => showProduct(data));
+  .then((res) => res.json())
+  .then((data) => showProducts);
+
+function showProducts(products) {
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+
+  const template = document.querySelector("#productSmallTemplate");
+
+  const copy = template.cloneNode(true);
+
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  document.querySelector("main").appendChild(copy);
+}
